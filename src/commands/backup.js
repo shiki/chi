167e3bdb74f4d897bcc05510b4089049869e7fbc
Shiki <jayson@basanes.net>
2018-01-01 10:22:40 -0700

    Backup: Make destination path configurable

--- src/commands/backup.js
+++ src/commands/backup.js
@@ -1,7 +1,8 @@
-import yaml from 'js-yaml'
 import fs from 'fs'
 import os from 'os'
 import path from 'path'
+
+import yaml from 'js-yaml'
 import shell from 'shelljs'
 import moment from 'moment'
 import untildify from 'untildify'
@@ -9,34 +10,74 @@ import prog from 'caporal'
 
 prog.command('backup', 'Backup folders defined in .chi.config.yml').action(handleAction)
 
-function handleAction(args, options, logger) {
-  const targetBasePath = `${os.homedir()}/Desktop`
-  const config = yaml.safeLoad(fs.readFileSync(`${os.homedir()}/.chi.config.yaml`, 'utf8'))
-  config.backup.paths.forEach(pathDef => {
-    let folderPath = null
-    let zipFileName = null
-    if (typeof pathDef === 'string') {
-      folderPath = pathDef
-      zipFileName = path.basename(folderPath)
-    } else {
-      folderPath = pathDef.path
-      zipFileName = pathDef.name
-    }
+async function handleAction(args, options, logger) {
+  const config = yaml.safeLoad(fs.readFileSync(`${__dirname}/../../.chi.config.yaml`, 'utf8'))
+  const destinationPath = untildify(config.backup.destination)
+  if (!destinationPathIsValid(destinationPath, logger)) {
+    throw new Error(`Destination path ${destinationPath} is not accessible. Make sure that it exists and is writeable.`)
+  }
 
-    folderPath = untildify(folderPath)
-
-    logger.info(`Processing: ${folderPath}`)
-
-    // Execute git fetch if the directory is a git dir
-    shell.cd(folderPath)
-    if (shell.test('-d', '.git')) {
-      logger.info('Executing: git fetch')
-      shell.exec('git fetch')
-    }
-
-    const parentPath = path.dirname(folderPath)
-    const zipFilePath = `${targetBasePath}/${zipFileName}-${moment().format('YYYY-MM-DD-H-mm-ss')}.tar.gz`
-    logger.info(`Compressing to ${zipFilePath}`)
-    shell.exec(`tar -zcf "${zipFilePath}" -C "${parentPath}" "${path.basename(folderPath)}"`)
+  config.backup.source.forEach(sourceItem => {
+    const { sourcePath, zipFileName } = sourceItemConfigToPathAndZipFileName(sourceItem)
+    backup({ sourcePath, zipFileName, destinationPath, logger })
   })
+}
+
+function backup({ sourcePath, zipFileName, destinationPath, logger }) {
+  logger.info(`Processing: ${sourcePath}`)
+
+  // Execute git fetch if the directory is a git dir
+  shell.cd(sourcePath)
+  if (shell.test('-d', '.git')) {
+    logger.info('Executing: git fetch')
+    shell.exec('git fetch')
+  }
+
+  const sourceParentPath = path.dirname(sourcePath)
+
+  const zipFileNameWithTimestamp = `${zipFileName}-${moment().format('YYYY-MM-DD-H-mm-ss')}.tar.gz`
+  const tempZipFilePath = path.join(createTempFolderPath(), zipFileNameWithTimestamp)
+
+  logger.info(`Compressing to ${tempZipFilePath}`)
+  shell.exec(`tar -zcf "${tempZipFilePath}" -C "${sourceParentPath}" "${path.basename(sourcePath)}"`)
+
+  const targetZipFilePath = path.join(destinationPath, zipFileNameWithTimestamp)
+
+  logger.info(`Moving to ${targetZipFilePath}`)
+  shell.mv(tempZipFilePath, targetZipFilePath)
+}
+
+function sourceItemConfigToPathAndZipFileName(sourceItem) {
+  let sourcePath = null
+  let zipFileName = null
+  if (typeof sourceItem === 'string') {
+    sourcePath = untildify(sourceItem)
+    zipFileName = path.basename(sourcePath)
+  } else {
+    sourcePath = untildify(sourceItem.path)
+    zipFileName = sourceItem.name
+  }
+
+  return { sourcePath, zipFileName }
+}
+
+function destinationPathIsValid(destinationPath, logger) {
+  try {
+    const isDirectory = fs.lstatSync(destinationPath).isDirectory()
+    if (!isDirectory) {
+      return false
+    }
+
+    fs.accessSync(destinationPath, fs.constants.W_OK)
+    return true
+  } catch (e) {
+    logger.error(e.message)
+    return false
+  }
+}
+
+function createTempFolderPath() {
+  const folderPath = path.join(os.tmpdir(), 'chi-backups')
+  shell.mkdir('-p', folderPath)
+  return folderPath
 }
